4966a1386e00d9704a58aef05edff29e8822dc4b
abinesh <abinesh.bhuvaneshwaran@applivon.com>
2024-05-27 14:16:45 +0800

    UC-36

--- flemings_base/models/models.py
+++ flemings_base/models/models.py
@@ -282,19 +282,6 @@ class FlemingsSalesOrder(models.Model):
                 })
         return res
 
-    @api.depends('order_line.invoice_lines')
-    def _get_invoiced(self):
-        # The invoice_ids are obtained thanks to the invoice lines of the SO
-        # lines, and we also search for possible refunds created directly from
-        # existing invoices. This is necessary since such a refund is not
-        # directly linked to the SO.
-        for order in self:
-            invoices = order.order_line.invoice_lines.move_id.filtered(
-                lambda r: r.move_type in ('out_invoice', 'out_refund'))
-            invoices += self.env['account.move.line'].sudo().search([('sale_id', '=', order.id)]).mapped('move_id')
-            order.invoice_ids = invoices
-            order.invoice_count = len(invoices)
-
     @api.depends('invoice_status', 'order_line', 'order_line.invoice_status', 'picking_ids', 'picking_ids.state', 'invoice_ids')
     def _compute_fg_invoice_status(self):
         for record in self:
@@ -705,6 +692,7 @@ class FlemingsStockPicking(models.Model):
                 invoice_line_vals.append((0, 0, {
                     'picking_id': record.id,
                     'sale_id': record.sale_id.id,
+                    'sale_line_ids': [(6, 0, move_line.sale_line_id.ids or [])],
                     'product_id': move_line.product_id.id,
                     'name': move_line.product_id.get_product_multiline_description_sale(),
                     'quantity': move_line.quantity_done,
