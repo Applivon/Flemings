dda2de83ae4740ad749ca562f7b56afa67704f33
abinesh <abinesh.bhuvaneshwaran@applivon.com>
2024-07-16 18:11:41 +0800

    UC-53

--- flemings_base/wizard/work_order_summary_report.py
+++ flemings_base/wizard/work_order_summary_report.py
@@ -62,11 +62,12 @@ class WorkOrderSummaryReport(models.TransientModel):
                                     lambda x: x.id in type_grouped_product_ids.ids)
 
                                 final_mo_products = mo_orders.filtered(lambda x: x.product_id.id in size_grouped_product_ids.ids)
-                                size_attribute_list.append({
-                                    'size_name': size_value_id.name,
-                                    'grouped_qty': sum(final_mo_products.mapped('product_qty')) or 0,
-                                })
-                                remarks += ' ' + str(' '.join([i for i in final_mo_products.filtered(lambda x: x.remarks).mapped('remarks')]))
+                                if sum(final_mo_products.mapped('product_qty')):
+                                    size_attribute_list.append({
+                                        'size_name': size_value_id.name,
+                                        'grouped_qty': sum(final_mo_products.mapped('product_qty')) or 0,
+                                    })
+                                    remarks += ' ' + str(' '.join([i for i in final_mo_products.filtered(lambda x: x.remarks).mapped('remarks')]))
 
                         type_attribute_list.append({
                             'type_name': type_value_id.name,
