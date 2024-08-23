0117afc899aec507fbee08c8eb5c12558e84ca9b
abinesh <abinesh.bhuvaneshwaran@applivon.com>
2024-08-23 15:36:29 +0800

    Modifications to be performed in COGS report

--- flemings_base/wizard/cogs_detailed_report.py
+++ flemings_base/wizard/cogs_detailed_report.py
@@ -67,7 +67,9 @@ class FlemingsCOGSDetailedReportXlsx(models.AbstractModel):
             sheet.set_column('B:B', 24)
             sheet.set_column('C:C', 35)
             sheet.set_column('D:D', 30)
-            sheet.set_column('E:E', 22)
+            sheet.set_column('E:E', 10)
+            sheet.set_column('F:F', 22)
+            sheet.set_column('G:G', 22)
 
             date_from = datetime.strftime(obj.from_date, '%d-%m-%Y')
             date_to = datetime.strftime(obj.to_date, '%d-%m-%Y')
@@ -85,7 +87,7 @@ class FlemingsCOGSDetailedReportXlsx(models.AbstractModel):
             sheet.write(row, 1, str(date_to), align_left)
 
             row += 2
-            titles = ['S.No', 'SKU', 'Product Name', 'DO No.', 'COGS Value']
+            titles = ['S.No', 'SKU', 'Product Name', 'DO No.', 'Qty', 'Unit Cost Price', 'COGS Value']
             for index in range(0, len(titles)):
                 sheet.write(row, index, titles[index], align_bold_center)
 
@@ -103,26 +105,37 @@ class FlemingsCOGSDetailedReportXlsx(models.AbstractModel):
                 domain += [('product_id', 'in', obj.product_ids.ids or [])]
 
             stock_valuation_env = self.env['stock.valuation.layer'].sudo()
-            total_sales_ids = stock_valuation_env.search(domain)
-            sold_products = list(set(total_sales_ids.mapped('product_id')))
+            total_sales_ids = stock_valuation_env.search(domain, order='create_date')
+            # sold_products = list(set(total_sales_ids.mapped('product_id')))
 
             row += 1
             sno = 1
-            for product_id in sold_products:
-                product_sales = stock_valuation_env.search(
-                    [('product_id', '=', product_id.id), ('id', 'in', total_sales_ids.ids or [])])
-
-                do_nos = ', '.join([i for i in product_sales.mapped('reference')])
-                cogs_value = sum(product_sales.mapped('value')) or 0
-
+            # for product_id in sold_products:
+            #     product_sales = stock_valuation_env.search(
+            #         [('product_id', '=', product_id.id), ('id', 'in', total_sales_ids.ids or [])])
+            #
+            #     do_nos = ', '.join([i for i in product_sales.mapped('reference')])
+            #     cogs_value = sum(product_sales.mapped('value')) or 0
+            #
+            #     sheet.write(row, 0, str(sno), align_center)
+            #     sheet.write(row, 1, str(product_id.default_code or ''), align_left)
+            #     sheet.write(row, 2, str(product_id.name or ''), align_left)
+            #     sheet.write(row, 3, str(do_nos or ''), align_left)
+            #     sheet.write(row, 4, str(obj.sgd_currency_id.symbol or '') + ' ' + str('%.2f' % abs(cogs_value) or 0), align_right)
+            #
+            #     row += 1
+            #     sno += 1
+            for line in total_sales_ids:
                 sheet.write(row, 0, str(sno), align_center)
-                sheet.write(row, 1, str(product_id.default_code or ''), align_left)
-                sheet.write(row, 2, str(product_id.name or ''), align_left)
-                sheet.write(row, 3, str(do_nos or ''), align_left)
-                sheet.write(row, 4, str(obj.sgd_currency_id.symbol or '') + ' ' + str('%.2f' % abs(cogs_value) or 0), align_right)
+                sheet.write(row, 1, str(line.product_id.default_code or ''), align_left)
+                sheet.write(row, 2, str(line.product_id.name or ''), align_left)
+                sheet.write(row, 3, str(line.reference or ''), align_left)
+                sheet.write(row, 4, str('%.2f' % abs(line.quantity) or 0), align_right)
+                sheet.write(row, 5, str(line.currency_id.symbol or '') + ' ' + str('%.2f' % line.unit_cost or 0), align_right)
+                sheet.write(row, 6, str(line.currency_id.symbol or '') + ' ' + str('%.2f' % abs(line.value) or 0), align_right)
 
                 row += 1
                 sno += 1
 
-            if not sold_products:
+            if not total_sales_ids:
                 sheet.merge_range(row + 1, 0, row + 1, 4, 'No Record(s) found', workbook.add_format({'font_name': 'Arial', 'align': 'center', 'valign': 'vcenter', 'bold': True, 'text_wrap': True}))
