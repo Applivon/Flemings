326a85d1323195570a07b79ba590db7aa2663b79
abinesh <abinesh.bhuvaneshwaran@applivon.com>
2024-12-24 14:00:54 +0800

    Description field to only show Product Name in Quotation,DO, Invoice and CN

--- flemings_base/models/models.py
+++ flemings_base/models/models.py
@@ -768,6 +768,27 @@ class FlemingsSalesAccountMoveLines(models.Model):
     lot_id = fields.Many2one('stock.lot', string='Lot/Serial No.', copy=False)
     product_tracking = fields.Selection(related='product_id.tracking', string='Tracking')
 
+    @api.depends('product_id')
+    def _compute_name(self):
+        for line in self:
+            if line.display_type == 'payment_term':
+                if line.move_id.payment_reference:
+                    line.name = line.move_id.payment_reference
+                elif not line.name:
+                    line.name = ''
+                continue
+            if not line.product_id or line.display_type in ('line_section', 'line_note'):
+                continue
+            if line.partner_id.lang:
+                product = line.product_id.with_context(lang=line.partner_id.lang)
+            else:
+                product = line.product_id
+
+            values = []
+            if product.partner_ref:
+                values.append(product.variant_name)
+            line.name = '\n'.join(values)
+
 
 class FlemingsProductTemplate(models.Model):
     _inherit = 'product.template'
