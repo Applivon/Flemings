a8c0bc47f645d01b1d83d4211da1214b64d9b0ae
abinesh <abinesh.bhuvaneshwaran@applivon.com>
2024-12-27 19:28:14 +0800

    T&C from Accounting->Settings should not be shown in Sale Order

--- flemings_base/models/models.py
+++ flemings_base/models/models.py
@@ -431,6 +431,21 @@ class FlemingsSalesOrder(models.Model):
             else:
                 record.can_user_edit_qty = False
 
+    @api.depends('partner_id')
+    def _compute_note(self):
+        use_invoice_terms = self.env['ir.config_parameter'].sudo().get_param('account.use_invoice_terms')
+        if not use_invoice_terms:
+            return
+        # for order in self:
+        #     order = order.with_company(order.company_id)
+        #     if order.terms_type == 'html' and self.env.company.invoice_terms_html:
+        #         baseurl = html_keep_url(order._get_note_url() + '/terms')
+        #         context = {'lang': order.partner_id.lang or self.env.user.lang}
+        #         order.note = _('Terms & Conditions: %s', baseurl)
+        #         del context
+        #     elif not is_html_empty(self.env.company.invoice_terms):
+        #         order.note = order.with_context(lang=order.partner_id.lang).env.company.invoice_terms
+
 
 class FlemingsPurchaseOrder(models.Model):
     _inherit = 'purchase.order'
